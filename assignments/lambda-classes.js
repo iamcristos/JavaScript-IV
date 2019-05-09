41a29196331f0c181d0eecf3d06a5a5d54ba79ae
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-05-09 19:00:03 +0100

    implemented strech goals

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -14,50 +14,6 @@ class Person {
 
 
 /**
- * @class Students
- * @extends {Person}
- */
-class Students extends Person {
-    constructor ({name, age, location, gender, previouBackground, className,favSubjects}) {
-        super({name, age, location, gender});
-        this.previouBackground = previouBackground;
-        this.className = className;
-        this.favSubjects = favSubjects;
-    }
-
-
-    /**
-     * @returns a string listing students favorite subject one by one
-     * @memberOf Students
-     */
-    listsSubjects () {
-        console.log(this.favSubjects);
-        return this.favSubjects.forEach((favSubject, i) => console.log(`${i+1}: ${favSubject};`));
-    }
-
-
-    /**
-     * @param {any} subject 
-     * @returns student PR on a subject
-     * @memberOf Students
-     */
-    PRAssignment (subject) {
-        return `${this.name} has submitted a PR for ${subject}`
-    }
-
-
-    /**
-     * @param {any} subject 
-     * @returns sprint challange a student starts
-     * @memberOf Students
-     */
-    sprintChallenge (subject) {
-        return `${this.name} has begun sprint challange on ${subject}`;
-    }
-}
-
-
-/**
  * @class Instructor
  * @extends {Person}
  */
@@ -88,8 +44,77 @@ class Instructor extends Person {
     grade (student, subject) {
        return `${student.name} recieves a perfect score on ${subject}`;
     }
+
+    points (max) {
+        let studentGrade = Math.floor(Math.random() * Math.floor(max))
+        if (studentGrade <=75) {
+           return studentGrade += Math.random() * 10
+        } else {
+           return studentGrade -= Math.random() * 1.3
+        }
+    }
+
 }
 
+/**
+ * @class Students
+ * @extends {Person}
+ */
+class Students extends Person {
+    constructor ({name, age, location, gender, previouBackground, className,favSubjects}) {
+        super({name, age, location, gender});
+        this.previouBackground = previouBackground;
+        this.className = className;
+        this.favSubjects = favSubjects;
+        this.grade = Math.floor(Math.random() * Math.floor(100));
+    }
+
+
+    /**
+     * @returns a string listing students favorite subject one by one
+     * @memberOf Students
+     */
+    listsSubjects () {
+        return this.favSubjects.forEach((favSubject, i) => console.log(`${i+1}: ${favSubject};`));
+    }
+
+
+    /**
+     * @param {any} subject 
+     * @returns student PR on a subject
+     * @memberOf Students
+     */
+    PRAssignment (subject) {
+        return `${this.name} has submitted a PR for ${subject}`
+    }
+
+
+    /**
+     * @param {any} subject 
+     * @returns sprint challange a student starts
+     * @memberOf Students
+     */
+    sprintChallenge (subject) {
+        return `${this.name} has begun sprint challange on ${subject}`;
+    }
+
+    graduate (instructor) {
+        if (this.grade >= 70) {
+            return `congratulations ${this.name} you have graduated.`
+        } else {
+                let i = 0;
+                while(i<1) {
+                    i++
+                    instructor.points(75);
+                    if (this.grade >= 70) {
+                        return `congratulations ${this.name} you have graduated.`
+                    } else {
+                        return `${this.name} cannot graduate`
+                    }
+                }
+            } 
+        }
+}
 
 /**
  * @class ProjectManagers
@@ -130,9 +155,8 @@ const fred = new Instructor({
 
   console.log(fred.demo('javascript'))
   
-
   const damola = new Students({
-    name: 'Fred',
+    name: 'Damola',
     location: 'Bedrock',
     age: 37,
     gender: 'male',
@@ -141,7 +165,13 @@ const fred = new Instructor({
     catchPhrase: `Don't forget the homies`,
     gradClassName: 'CS1',
     favInstructor: 'Sean'
+    // grade: 50
   });
+  console.log(fred.points(100))
   console.log(fred.grade(damola,'java'))
+  console.log(damola.graduate(fred))
   console.log(damola.listsSubjects())
   console.log(damola.sprintChallenge('java'))
+  
+
+  
